39ff9876cc01deec118ade31d4d46638285bf1e7
Aman Ajeet Yadav <amanjetasb@gmail.com>
2025-03-15 23:47:45 +0530

    Time: 470 ms (26.53%) | Memory: 20.6 MB (28.57%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def minCapability(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def canRob(capability):
+            count =0
+            i = 0
+            while i< len(nums):
+                if nums[i]<=capability:
+                    count +=1
+                    i+=1
+                i+=1
+            return count >= k
+
+        left , right = min(nums),max(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if canRob(mid):
+                right = mid  
+            else:
+                left = mid + 1  
+        
+        return left        
