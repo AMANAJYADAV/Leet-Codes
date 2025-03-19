1de44a0e8980c1cd59d2bfdd99c75abe0635174b
Aman Ajeet Yadav <amanjetasb@gmail.com>
2025-03-19 23:58:46 +0530

    Time: 2 ms (76.02%) | Memory: 57.1 MB (12.07%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+};
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in map) {
+            let topElement = stack.length > 0 ? stack.pop() : '#';
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+};
